7e02dfe169e597ba4b238c138a510cfc890931bb
kitsune4864 <necsinete9050@gmail.com>
2024-10-25 16:47:49 +0900

    add : InputKeyCode
    던지는 게이지 수정, 카메라 수정

---
+++ Assets/04.Code/Scripts/KBS/KccTest.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class KccTest : MonoBehaviour
+{
+    private CharacterController cc;
+    private Animator animator;
+
+    public float moveSpeed = 5f;
+    public float rotSpeed = 200f;
+
+    private float mx = 0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerRotate();
+    }
+
+    private void PlayerMove()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0f, v);
+        dir.Normalize();
+        dir = Camera.main.transform.TransformDirection(dir);
+        
+        cc.Move(moveSpeed * Time.deltaTime * dir);
+        
+        if (Mathf.Approximately(h, 0f) && Mathf.Approximately(v, 0f))
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+        }
+    }
+
+    private void PlayerRotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        mx += mouseX * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0f, mx, 0f);
+    }
+}
