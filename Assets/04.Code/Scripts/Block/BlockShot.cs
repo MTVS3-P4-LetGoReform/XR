335d9169130ef6166e8620fd0f8a6e34c1a6f7e1
kitsune4864 <necsinete9050@gmail.com>
2024-10-28 15:35:20 +0900

    fix : Camera Toggle
    줌 상태에서만 던지기 및 블록설치가 가능하도록 수정

--- Assets/04.Code/Scripts/Block/BlockShot.cs
+++ Assets/04.Code/Scripts/Block/BlockShot.cs
@@ -32,55 +32,56 @@ public class BlockShot : MonoBehaviour
 
     private void ThrowBlock()
     {
-        if (Input.GetKey(KeyCode.F))
+        if (KccCameraTest.togglePov)
         {
-            isKeyPressed = true;
-            chargeGauge.gameObject.SetActive(true);
+            if (Input.GetKey(KeyCode.F))
+            {
+                isKeyPressed = true;
+                chargeGauge.gameObject.SetActive(true);
+            }
+
+            if (isKeyPressed)
+            {
+                if (chargeGauge.value < chargeGauge.maxValue)
+                {
+                    chargeGauge.value += increaseSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    chargeGauge.value = chargeGauge.maxValue;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                isKeyPressed = false;
+                chargeGauge.gameObject.SetActive(false);
+                currentGauge = chargeGauge.value;
+                chargeGauge.value = chargeGauge.minValue;
+                if (SharedBlockData.BlockNumber > 0)
+                {
+                    GameObject block = Instantiate(ShotBlock, blockShotPoint.transform.position, Quaternion.identity);
+                    Debug.Log(block.transform.position);
+                    Rigidbody rB = block.GetComponent<Rigidbody>();
+
+                    Vector3 throwDirection = (camera.transform.forward * 20) + (Vector3.up * 10);
+
+                    rB.AddForce(throwDirection * currentGauge, ForceMode.Impulse);
+                    Debug.Log(currentGauge);
+
+                    SharedBlockData.BlockNumber -= 1;
+                    blockCountText.text = $"{SharedBlockData.BlockNumber}";
+
+                    animator.SetTrigger("IsThrowing");
+
+                }
+                else
+                {
+                    Debug.Log("No Block!!");
+                }
+            }
+
+
         }
-
-        if (isKeyPressed)
-        {
-            if (chargeGauge.value < chargeGauge.maxValue)
-            {
-                chargeGauge.value += increaseSpeed * Time.deltaTime;
-            }
-            else
-            {
-                chargeGauge.value = chargeGauge.maxValue;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            isKeyPressed = false;
-            chargeGauge.gameObject.SetActive(false);
-            currentGauge = chargeGauge.value;
-            chargeGauge.value = chargeGauge.minValue;
-            if (SharedBlockData.BlockNumber > 0)
-            {
-                GameObject block = Instantiate(ShotBlock, blockShotPoint.transform.position, Quaternion.identity);
-                Debug.Log(block.transform.position);
-                Rigidbody rB = block.GetComponent<Rigidbody>();
-
-                Vector3 throwDirection = (camera.transform.forward * 20) + (Vector3.up * 10);
-                
-                rB.AddForce(throwDirection * currentGauge, ForceMode.Impulse); 
-                Debug.Log(currentGauge);
-                
-                SharedBlockData.BlockNumber -= 1;
-                blockCountText.text = $"{SharedBlockData.BlockNumber}";
-
-                animator.SetTrigger("IsThrowing");
-
-            }
-            else
-            {
-                Debug.Log("No Block!!");
-            }
-        }
-
-       
     }
-    
-    
 }
