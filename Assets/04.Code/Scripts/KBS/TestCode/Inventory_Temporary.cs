162eede15efd0681c9b4ba7dce00024ca9b0534d
kitsune4864 <necsinete9050@gmail.com>
2024-10-29 15:43:17 +0900

    Add : MinionsPrefab

--- Assets/04.Code/Scripts/KBS/TestCode/Inventory_Temporary.cs
+++ Assets/04.Code/Scripts/KBS/TestCode/Inventory_Temporary.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class Inventory_temporary : MonoBehaviour
 {
     public Image inventory;
+    public Button closeButton;
+    public GameObject minionsPrefab;
     
     private bool isInvenPush = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,5 +21,16 @@ public class Inventory_temporary : MonoBehaviour
         {
             inventory.gameObject.SetActive(true);
         }
+    }
+
+    public void ChooseStatueOnClick()
+    {
+        Vector3 point = new Vector3(5, 26, -17);
+        Instantiate(minionsPrefab, point, Quaternion.identity);
+    }
+
+    public void CloseButtonOnClick()
+    {
+        inventory.gameObject.SetActive(false);
     }
 }
