bc62ad7d039e4522322d0c84ee8122b521791041
이서영 <88702441+WillowSY@users.noreply.github.com>
2024-11-12 14:25:10 +0900

    fix

--- Assets/04.Code/Scripts/LSY/DB/StatueInventoryDB.cs
+++ Assets/04.Code/Scripts/LSY/DB/StatueInventoryDB.cs
@@ -1,5 +1,44 @@
-﻿public class StatueInventoryDB
+﻿using UnityEngine;
+
+public class StatueInventoryDB: MonoBehaviour
 {
+    private string _userId;
+    private string[] hasModel;
+    private string[] hasModelName;
+    private StorageDatabase _storageDatabase;
+    public WebApiData webApiData;
     
+    private void Start()
+    {
+        _storageDatabase = new StorageDatabase(webApiData);
+        if (UserData.Instance ==null)
+            return;
+        _userId = UserData.Instance.UserId;
+
+        LoadUserModelData();
         
+    }
+
+    public void LoadUserModelData()
+    {
+        RealtimeDatabase.GetModelList(_userId, modelList =>
+            {
+                if (modelList != null)
+                {
+                    Debug.Log($"StatueInventoryDB : modelList - {modelList}");
+                    foreach (var model in modelList.models)
+                    {
+                        Debug.Log($"StatueInventoryDB : model - {model}");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("StaueInventoryDB : 모델리스트 데이터를 찾을 수 없음.");
+                }
+            },
+            error => { Debug.LogError($"StatueInventoryDB : 데이터 읽는 중 오류 -{error.Message}"); }
+        );
+    }
+
+    
 }
