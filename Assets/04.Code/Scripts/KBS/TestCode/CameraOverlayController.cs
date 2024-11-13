e5520e6855255fafb34f6a0565a95e7475b7d653
kitsune4864 <necsinete9050@gmail.com>
2024-11-13 18:15:53 +0900

    add : UICameraOverlay

--- Assets/04.Code/Scripts/KBS/TestCode/CameraOverlayController.cs
+++ Assets/04.Code/Scripts/KBS/TestCode/CameraOverlayController.cs
@@ -1,41 +1,46 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class CameraOverlayController : MonoBehaviour
 {
     public Camera baseCamera;       // Base 카메라
     private Camera overlayCamera;
     private GameObject newOverlayCamera;
+
+    private Coroutine currentCoroutine;
+
+    private bool isCameraOn = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
-        
+        currentCoroutine = StartCoroutine(FindCamera());
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(FindCamera());
+        
     }
     
     
     private IEnumerator FindCamera()
     {
-        GameObject cameraObject = null;
-
-        while (cameraObject == null)
+        newOverlayCamera = null;
+        
+        while (newOverlayCamera == null)
         {
-            cameraObject = GameObject.FindWithTag("UICamera");
+            newOverlayCamera = GameObject.FindWithTag("UICamera");
 
-            if (cameraObject == null)
+            if (newOverlayCamera == null)
             {
                 yield return new WaitForSeconds(0.5f);
             }
             else
             {
-                overlayCamera = cameraObject.GetComponent<Camera>();
+                overlayCamera = newOverlayCamera.GetComponent<Camera>();
         
                 var baseCameraData = baseCamera.GetUniversalAdditionalCameraData();
         
@@ -44,8 +49,13 @@ public class CameraOverlayController : MonoBehaviour
             
                 // Base 카메라의 스택에 Overlay 카메라를 추가합니다.
                 baseCameraData.cameraStack.Add(overlayCamera);
+
+                isCameraOn = true;
             }
+            
         }
+        
+        
         
     }
 }
