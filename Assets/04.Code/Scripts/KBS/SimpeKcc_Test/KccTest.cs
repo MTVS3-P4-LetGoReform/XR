2b8f77c204a0813e190b82c008aa7ae3b9b96595
kitsune4864 <necsinete9050@gmail.com>
2024-11-14 07:00:21 +0900

    add motion : jump, walking audio

--- Assets/04.Code/Scripts/KBS/SimpeKcc_Test/KccTest.cs
+++ Assets/04.Code/Scripts/KBS/SimpeKcc_Test/KccTest.cs
@@ -69,6 +69,7 @@ public class KccTest : NetworkBehaviour
         if (Input.GetButtonDown("Jump") && networkCC.Grounded)
         {
             _jump = true;
+            animator.SetTrigger("IsJumping");
         }
     }
 
