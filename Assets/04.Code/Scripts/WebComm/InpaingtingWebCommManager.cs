a33bbfb736e8db9d541aec836238e649110fca80
이서영 <88702441+WillowSY@users.noreply.github.com>
2024-12-14 23:41:48 +0900

    feat : 옷갈아입기 통신 붙임

--- Assets/04.Code/Scripts/WebComm/InpaingtingWebCommManager.cs
+++ Assets/04.Code/Scripts/WebComm/InpaingtingWebCommManager.cs
@@ -4,6 +4,7 @@ using Cysharp.Threading.Tasks;
 using GLTFast;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class InpaingtingWebCommManager : MonoBehaviour
@@ -20,11 +21,14 @@ public class InpaingtingWebCommManager : MonoBehaviour
 
     private InpaintingGen _inpaintingGen;
     private ImageDownload _imageDownload;
+    private ModelGen _modelGen;
     private ModelDown _modelDown;
     
     public Image previewImage;
     public Button transferBtn;
     public Button addBtn;
+
+    public string modelName;
 
     public void Start()
     {
@@ -47,7 +51,7 @@ public class InpaingtingWebCommManager : MonoBehaviour
 
     public async UniTask DoTransferring()
     {
-        await InpaintingModelDownAdd();
+        await InpaintingModelGenDownAdd();
     }
     public async UniTask InpaintingGenImageDown()
     {
@@ -62,26 +66,36 @@ public class InpaingtingWebCommManager : MonoBehaviour
 
     }
 
-    public async UniTask InpaintingModelDownAdd()
+    public async UniTask InpaintingModelGenDownAdd()
     {
+        _modelGen = new ModelGen(webApiData);
+
+        await _modelGen.RequestModelGen(_inpaintingGen._inpaintingRes.filenames, _inpaintingGen._inpaintingRes.id);
         // StorageDatabase 초기화
         StorageDatabase.InitializStorageDatabase(webApiData, debugModeData);
+        Debug.Log($"InpaintingModelGenDownAdd : {_modelGen._modelGenRes.model_filename}");
         
-        // 모델 다운로드
-        await StorageDatabase.DownModel(_inpaintingGen._inpaintingRes.id);
-
-        string modelId = _inpaintingGen._inpaintingRes.id;
-        // PNG 데이터를 Sprite로 변환
-        Sprite sprite = SpriteConverter.ConvertFromPNG(_inpaintingGen._inpaintingRes.filenames);
-        // Firebase에서 모델 정보 읽어오기
-        var modelInfo = await RealtimeDatabase.ReadDataAsync<ModelInfo>($"user_land/{modelId}/create_3d_name");
-        if (modelInfo == null)
+        if (_modelGen.request.result == UnityWebRequest.Result.Success)
         {
-            Debug.LogError("InpaintingWebCommManager : ModelInfo is null");
+            modelName = _modelGen._modelGenRes.model_filename;
+            webApiData.ModelName = _modelGen._modelGenRes.model_filename;
+            // ModelDown _modelDown = new ModelDown(webApiData);
+            // yield return StartCoroutine(_modelDown.DownloadGLBFile(modelName));
+            // _modelDown.LoadAndInstantiateGLB(modelName);
+            //FIXME : 가이드라인 생성으로 추후 변경
+
+                
         }
         
+        // 모델 다운로드
+        await StorageDatabase.DownModel(modelName);
+
+        //string modelId = _inpaintingGen._inpaintingRes.id;
+        // PNG 데이터를 Sprite로 변환
+        Sprite sprite = SpriteConverter.ConvertFromPNG(_inpaintingGen._inpaintingRes.filenames);
+        
         // GLTF 모델 로드
-        string modelPath = Path.Combine(Application.persistentDataPath, "Models", modelInfo.create_3d_name);
+        string modelPath = Path.Combine(Application.persistentDataPath, "Models", modelName);
         GltfImport gltfImport = await GltfLoader.LoadGLTF(modelPath);
         
         statueInventoryController.AddStatueToInven(_inpaintingGen._inpaintingRes.id, sprite, gltfImport);
